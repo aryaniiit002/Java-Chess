979c9d00fa3db46f7e4422dd2835bc9f2f71be15
Aryaniiit002 <aryanbindal2015@gmail.com>
2021-06-21 12:44:31 +0530

    Commit #46

--- src/com/chess/engine/player/ai/MiniMax.java
+++ src/com/chess/engine/player/ai/MiniMax.java
@@ -2,6 +2,7 @@ package com.chess.engine.player.ai;
 
 import com.chess.engine.board.Board;
 import com.chess.engine.board.Move;
+import com.chess.engine.player.MoveTransition;
 
 public class MiniMax implements MoveStrategy {
 
@@ -27,5 +28,51 @@ public class MiniMax implements MoveStrategy {
     @Override
     public Move execute(Board board, int depth) {
         return null;
+    }
+
+    private int min(final Board board,
+                    final int depth) {
+        if(depth == 0) {
+            return this.evaluator.evaluate(board, depth);
+        }
+        /*if() {
+            return this.evaluator.evaluate(board, depth);
+        }*/
+        int lowestSeenValue = Integer.MAX_VALUE;
+
+        for (final Move move : board.currentPlayer().getLegalMoves()) {
+            final MoveTransition moveTransition = board.currentPlayer().makeMove(move);
+
+            if (moveTransition.getMoveStatus().isDone()) {
+                final int currentValue = max(moveTransition.getTransitionBoard(), depth - 1);
+
+                if (currentValue <= lowestSeenValue) {
+                    lowestSeenValue = currentValue;
+                }
+            }
+        }
+
+        return lowestSeenValue;
+    }
+
+    private int max(final Board board,
+                    final int depth) {
+        if(depth == 0) {
+            return this.evaluator.evaluate(board, depth);
+        }
+        /*if(isEndGameScenario(board)) {
+            return this.evaluator.evaluate(board, depth);
+        }*/
+        int highestSeenValue = Integer.MIN_VALUE;
+        for (final Move move : board.currentPlayer().getLegalMoves()) {
+            final MoveTransition moveTransition = board.currentPlayer().makeMove(move);
+            if (moveTransition.getMoveStatus().isDone()) {
+                final int currentValue = min(moveTransition.getTransitionBoard(), depth - 1);
+                if (currentValue >= highestSeenValue) {
+                    highestSeenValue = currentValue;
+                }
+            }
+        }
+        return highestSeenValue;
     }
 }
