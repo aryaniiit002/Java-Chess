a1af8853fbbb8afd1004d16e87a284030967be44
Aryaniiit002 <aryanbindal2015@gmail.com>
2021-06-21 14:16:45 +0530

    Commit #49

--- src/com/chess/engine/player/ai/StandardBoardEvaluator.java
+++ src/com/chess/engine/player/ai/StandardBoardEvaluator.java
@@ -26,18 +26,49 @@ public final class StandardBoardEvaluator implements BoardEvaluator {
     @VisibleForTesting
     private static int score(final Board board, final Player player,
                              final int depth) {
-        return pieceValue(player);
+        return pieceValue(player) + mobility(player) +
+            check(player) + checkMate(player, depth)
+            + castle(player);
+    }
+
+    /**
+     * How many legal moves does the player has.
+     *
+     * @param player player whose number of legal moves to find
+     * @return number of legal moves
+     */
+    private static int mobility(final Player player) {
+        return MOBILITY_MULTIPLIER * mobilityRatio(player);
+    }
+
+    private static int mobilityRatio(final Player player) {
+        return (int)(player.getLegalMoves().size() * 10.0f / player.getOpponent().getLegalMoves().size());
     }
 
     private static int pieceValue(final Player player) {
         int pieceValuationScore = 0;
         int numBishops = 0;
         for (final Piece piece : player.getActivePieces()) {
-            pieceValuationScore += piece.getPieceValue() + piece.locationBonus();
-            if(piece.getPieceType() == BISHOP) {
-                numBishops++;
-            }
+            pieceValuationScore += piece.getPieceValue();
         }
         return pieceValuationScore + (numBishops == 2 ? TWO_BISHOPS_BONUS : 0);
     }
+
+    private static int check(final Player player) {
+        return player.getOpponent().isInCheck() ? CHECK_BONUS : 0;
+    }
+
+    private static int checkMate(final Player player,
+                                   final int depth) {
+        return player.getOpponent().isInCheckMate() ? CHECK_MATE_BONUS  * depthBonus(depth) : check(player);
+    }
+
+    private static int depthBonus(final int depth) {
+        return depth == 0 ? 1 : 100 * depth;
+    }
+
+    private static int castle(final Player player) {
+        return player.isCastled() ? CASTLE_BONUS : 0;
+    }
+
 }
