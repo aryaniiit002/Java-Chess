cd72f1555c16ba9392124c16982717611a445c8b
Aryaniiit002 <aryanbindal2015@gmail.com>
2021-06-21 15:38:31 +0530

    Commit #50

--- src/com/chess/engine/player/ai/StandardBoardEvaluator.java
+++ src/com/chess/engine/player/ai/StandardBoardEvaluator.java
@@ -1,6 +1,7 @@
 package com.chess.engine.player.ai;
 
 import com.chess.engine.board.Board;
+import com.chess.engine.board.Move;
 import com.chess.engine.pieces.Piece;
 import com.chess.engine.player.Player;
 import com.google.common.annotations.VisibleForTesting;
@@ -13,54 +14,91 @@ public final class StandardBoardEvaluator implements BoardEvaluator {
     private static final int MOBILITY_MULTIPLIER = 5;
     private static final int ATTACK_MULTIPLIER = 1;
     private static final int TWO_BISHOPS_BONUS = 25;
+    private static final StandardBoardEvaluator INSTANCE = new StandardBoardEvaluator();
 
     private StandardBoardEvaluator() {
+    }
+
+    public static StandardBoardEvaluator get() {
+        return INSTANCE;
     }
 
     @Override
     public int evaluate(final Board board,
                         final int depth) {
-        return score(board, board.whitePlayer(), depth) - score(board, board.blackPlayer(), depth);
+        return score(board.whitePlayer(), depth) - score(board.blackPlayer(), depth);
+    }
+
+    public String evaluationDetails(final Board board, final int depth) {
+        return
+            "White Mobility : " + mobility(board.whitePlayer()) + "\n" +
+                "White kingThreats : " + kingThreats(board.whitePlayer(), depth) + "\n" +
+                "White attacks : " + attacks(board.whitePlayer()) + "\n" +
+                "White castle : " + castle(board.whitePlayer()) + "\n" +
+                "White pieceEval : " + pieceEvaluations(board.whitePlayer()) + "\n" +
+                "White pawnStructure : " + pawnStructure(board.whitePlayer()) + "\n" +
+                "---------------------\n" +
+                "Black Mobility : " + mobility(board.blackPlayer()) + "\n" +
+                "Black kingThreats : " + kingThreats(board.blackPlayer(), depth) + "\n" +
+                "Black attacks : " + attacks(board.blackPlayer()) + "\n" +
+                "Black castle : " + castle(board.blackPlayer()) + "\n" +
+                "Black pieceEval : " + pieceEvaluations(board.blackPlayer()) + "\n" +
+                "Black pawnStructure : " + pawnStructure(board.blackPlayer()) + "\n\n" +
+                "Final Score = " + evaluate(board, depth);
     }
 
     @VisibleForTesting
-    private static int score(final Board board, final Player player,
+    private static int score(final Player player,
                              final int depth) {
-        return pieceValue(player) + mobility(player) +
-            check(player) + checkMate(player, depth)
-            + castle(player);
+        return mobility(player) +
+               kingThreats(player, depth) +
+               attacks(player) +
+               castle(player) +
+               pieceEvaluations(player) +
+               pawnStructure(player);
     }
 
-    /**
-     * How many legal moves does the player has.
-     *
-     * @param player player whose number of legal moves to find
-     * @return number of legal moves
-     */
+    private static int attacks(final Player player) {
+        int attackScore = 0;
+        for(final Move move : player.getLegalMoves()) {
+            if(move.isAttack()) {
+                final Piece movedPiece = move.getMovedPiece();
+                final Piece attackedPiece = move.getAttackedPiece();
+                if(movedPiece.getPieceValue() <= attackedPiece.getPieceValue()) {
+                    attackScore++;
+                }
+            }
+        }
+        return attackScore * ATTACK_MULTIPLIER;
+    }
+
+    private static int pieceEvaluations(final Player player) {
+        int pieceValuationScore = 0;
+        int numBishops = 0;
+        for (final Piece piece : player.getActivePieces()) {
+            pieceValuationScore += piece.getPieceValue() + piece.locationBonus();
+            if(piece.getPieceType() == BISHOP) {
+                numBishops++;
+            }
+        }
+        return pieceValuationScore + (numBishops == 2 ? TWO_BISHOPS_BONUS : 0);
+    }
+
     private static int mobility(final Player player) {
         return MOBILITY_MULTIPLIER * mobilityRatio(player);
     }
 
     private static int mobilityRatio(final Player player) {
-        return (int)(player.getLegalMoves().size() * 10.0f / player.getOpponent().getLegalMoves().size());
+        return (int)((player.getLegalMoves().size() * 10.0f) / player.getOpponent().getLegalMoves().size());
     }
 
-    private static int pieceValue(final Player player) {
-        int pieceValuationScore = 0;
-        int numBishops = 0;
-        for (final Piece piece : player.getActivePieces()) {
-            pieceValuationScore += piece.getPieceValue();
-        }
-        return pieceValuationScore + (numBishops == 2 ? TWO_BISHOPS_BONUS : 0);
+    private static int kingThreats(final Player player,
+                                   final int depth) {
+        return player.getOpponent().isInCheckMate() ? CHECK_MATE_BONUS  * depthBonus(depth) : check(player);
     }
 
     private static int check(final Player player) {
         return player.getOpponent().isInCheck() ? CHECK_BONUS : 0;
-    }
-
-    private static int checkMate(final Player player,
-                                   final int depth) {
-        return player.getOpponent().isInCheckMate() ? CHECK_MATE_BONUS  * depthBonus(depth) : check(player);
     }
 
     private static int depthBonus(final int depth) {
@@ -70,5 +108,18 @@ public final class StandardBoardEvaluator implements BoardEvaluator {
     private static int castle(final Player player) {
         return player.isCastled() ? CASTLE_BONUS : 0;
     }
+
+    private static int pawnStructure(final Player player) {
+        return PawnStructureAnalyzer.get().pawnStructureScore(player);
+    }
+
+    private static int kingSafety(final Player player) {
+        final KingSafetyAnalyzer.KingDistance kingDistance = KingSafetyAnalyzer.get().calculateKingTropism(player);
+        return ((kingDistance.getEnemyPiece().getPieceValue() / 100) * kingDistance.getDistance());
+    }
+
+//    private static int rookStructure(final Player player) {
+//        return RookStructureAnalyzer.get().rookStructureScore(player);
+//    }
 
 }
